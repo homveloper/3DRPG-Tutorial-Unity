3b215300f1f5f9ffb90b7198a14ace7f78cdb462
homveloper <bum4496@naver.com>
2022-03-05 10:07:18 +0900

    개선 : Primary와 Secondary 버튼 추가

--- Assets/Scripts/Inputs/MouseInput.cs
+++ Assets/Scripts/Inputs/MouseInput.cs
@@ -8,11 +8,19 @@ namespace SD.Inputs
     public class MouseInput : MonoBehaviour
     {
         public event Action OnPrimary = delegate {};
+        public event Action OnSecondary = delegate {};
 
         private void Update()
         {
             if(Input.GetButtonDown("Primary")){
                 OnPrimary();
+            }
+
+            if (Input.GetButtonDown("Secondary"))
+            {
+                Vector3 worldPosition = Functional.GetMouseWorldPosition3D(true);
+                GetComponent<IMovePosition>()?.SetMovePosition(worldPosition);
+                OnSecondary();
             }
         }
     }
